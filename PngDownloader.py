85ed1cd1b9ea51911ce0bc914282b2deea7e9a67
aliceSmartCastle <shirahananami@gmail.com>
2025-11-28 04:42:23 -0200

    Add files via upload

--- PngDownloader.py
+++ PngDownloader.py
@@ -1,72 +1,110 @@
 import os.path
+import random
 import time
+import typing
 import urllib.error
 from threading import Thread
 from typing import List, Optional
-from urllib import request
-
+import requests
 from typing_extensions import Annotated
-from urllib.request import urlopen, Request
 import typer
-from FuncTip import CalculateAny, GetterMemory
+from FuncTip import GetterMemory
 from typing import Literal
 from time import perf_counter
 
-aqq=typer.Typer()
+aqq = typer.Typer()
+
 
 def pictureBar():
     for loading in range(100):
         yield loading
 
 
-def downloaderPng(filePathName: str, pngData: bytes):
+def downloaderPng(filePathName: str,ResponseContent:requests.models.Response):
     with open(filePathName, 'wb+') as pngFiles:
-        pngFiles.write(pngData)
-    print("file download is competed")
+     for chunk in ResponseContent.iter_content(chunk_size=2048):
+        pngFiles.write(chunk)
+def downloader_bar(progressbar,picturePath:str,Png:requests.models.Response):
+    process = 0
+    for _ in progressbar:
+        startTime = perf_counter()
+        downloaderPng(filePathName=picturePath, ResponseContent=Png)
+        end_time = perf_counter()
+        time.sleep(int(end_time - startTime) // 100)
+        process += 1
+
+
 
 
 def DownloaderFile(url: str, pathMethod: Literal['workPath', 'defaultPath', 'customerPath'] = 'defaultPath',
-                   customerPath: str = '', defaultPathName: str = 'pjskPng') -> None:
-    process = 0
+                   customerPath: str = '', defaultPathName: str = 'pjskPng', Referer_wedsite: str = 'None') -> None:
+    if Referer_wedsite == 'None':
+        Referer_wedsite = 'https://anime-pictures.net/'
+    errorInfo = None
     print(f"download from the the {url}")
-    pngData = None
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:145.0) Gecko/20100101 Firefox/145.0'
-    }
+    headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:145.0) Gecko/20100101 Firefox/145.0',
+               'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+               'Cookie': f'sitelang=en;kira={random.randint(1,100)};priors={random.randint(1000,1000000)}',
+               'Accept-Encoding': 'gzip,deflate,br,zstd',
+               'Accept-Language': "zh-CN;q=0.9,en-US;q=0.8,en;q=0.7",
+               'Referer': f"{Referer_wedsite}",
+               'Connection': 'keep-alive',
+               'Sec-Fetch-Dest': 'document',
+               'Sec-Fetch-Mode': 'navigate',
+               'Sec-Fetch-Site': 'same-site',
+               'Sec_GPC':'1',
+               }
+
+
+    if  'https://oimages.anime-pictures.net' in url:
+        FileName= url.split('-')[6].split('+')[0]+'.png'
+    else:
+       FileName = os.path.basename(url)
     try:
-     requests = Request(url, headers=headers)
-     with urlopen(requests) as Png:
-        with typer.progressbar(pictureBar(), length=100) as pro:
-            startTime = perf_counter()
-            pngData = Png.read()
-            endTime = perf_counter()
-            #print(f" spent time is:{int(endTime-startTime)//100}")
-            for _ in pro:
-                time.sleep(int(endTime - startTime) // 100)
-                process += 1
-            print(f"  download picture is compete")
-    except urllib.error.HTTPError as e:
-        print(f"Error: {e.code} {e.reason}")
-        if not pngData:
-            raise ValueError(f"Error: could not download the image from {url}")
 
-    FileName = os.path.basename(url)
+       with requests.get(url=url, headers=headers,timeout=30) as Png:
+            content_type = Png.headers.get('Content-Type', '')
+            if content_type in ['text/html']:
+                raise urllib.error.URLError("download assert can not is html")
+            else:
+             with typer.progressbar(pictureBar(), length=100,color=True) as pro:
 
-    match pathMethod:
-        case 'defaultPath':
-            picturePath = os.path.join(os.getcwd(), defaultPathName, FileName)
-            downloaderPng(picturePath, pngData)
-        case 'customerPath':
-            picturePath = os.path.join(os.getcwd(), customerPath, FileName)
-            downloaderPng(picturePath, pngData)
-        case 'workPath':
-            request.urlretrieve(url, FileName)
+                match pathMethod:
+                        case 'defaultPath':
+
+                            picturePath = os.path.join(os.getcwd(), defaultPathName, FileName)
+                            downloader_bar(progressbar=pro, picturePath=picturePath, Png=Png)
 
 
-@CalculateAny
+                        case 'customerPath':
+                            picturePath = os.path.join(os.getcwd(), customerPath, FileName)
+                            downloader_bar(progressbar=pro, picturePath=picturePath, Png=Png)
+                        case 'workPath':
+
+                            #downloaderPng(filePathName=os.path.join(os.getcwd(),FileName),ResponseContent=Png)
+                            downloader_bar(progressbar=pro, picturePath=os.path.join(os.getcwd(), FileName), Png=Png)
+
+
+
+    except requests.exceptions.ConnectionError as e:
+        errorInfo=e
+    finally:
+
+        if  'port=443' not  in str(errorInfo):
+         print("picture download successfully")
+        else:
+            print("picture download failed")
+
+
+
+
+
+
+
+
 # its Decorators can calculate all thread download of time
-def processPicture(urls: List[str], pathMethod: Literal['workPath', 'defaultPath', 'customerPath'] = 'defaultPath',
-                   customerPath: str = '', defaultPathName: str = 'pjskPng') -> None:
+def processPicture(urls: List[str], pathMethod: typing.Union[Literal['workPath', 'defaultPath', 'customerPath'], str] = 'defaultPath',
+                   customerPath: str = '', defaultPathName: str = 'pjskPng',Referer_wedsite: str = 'None') -> None:
     """
     the Thread Event and Lock is not needs,so use the '#' to comment it
     """
@@ -75,7 +113,7 @@ def processPicture(urls: List[str], pathMethod: Literal['workPath', 'defaultPath
         #pngLock = Lock()
 
         # PngThread = Thread(target=DownloaderFile, args=(urls[0], pngEvent))
-        multiThreadDownload = (Thread(target=DownloaderFile, args=(urls[i], pathMethod, customerPath, defaultPathName))
+        multiThreadDownload = (Thread(target=DownloaderFile, args=(urls[i], pathMethod, customerPath, defaultPathName, Referer_wedsite))
                                for i in
                                range(len(urls)))
         finish_list = []
@@ -84,11 +122,12 @@ def processPicture(urls: List[str], pathMethod: Literal['workPath', 'defaultPath
             pngThread.start()
         for pngFinish in finish_list:
             pngFinish.join()
-            print(f"all thread download picture successfully")
+
         GetterMemory()
         # watch the download codes use tge rss and vss
     else:
         print('not support empty list')
+
 
 @aqq.command()
 def main(fatalist: Annotated[Optional[List[str]], typer.Argument(help='url from list')] = None,
@@ -96,7 +135,8 @@ def main(fatalist: Annotated[Optional[List[str]], typer.Argument(help='url from 
                                                  prompt='please enter your method')] = 'defaultPath',
          customerPath: Annotated[str, typer.Option('--customer', '-C', prompt="please input the path")] = '',
          defaultPathName: Annotated[
-             str, typer.Option('--defaultPathName', '-D', prompt="please input the path name")] = 'pjskPng'
+             str, typer.Option('--defaultPathName', '-D', prompt="please input the default path name")] = 'pjskPng',
+         Referer_wedsite: Annotated[str, typer.Option('--Referer', '-R', prompt="please input the main wedsite url address")] = 'None'
          ):
     """
     workPath:download the picture in the work path\n
@@ -104,22 +144,28 @@ def main(fatalist: Annotated[Optional[List[str]], typer.Argument(help='url from 
     customerPath:download the picture in the customer path\n
 
 
+    :param Referer_wedsite:the main url address of your download picture wedsite\n
     :param fatalist: the list of urls\n
     :param PathMethod: the method of download the path\n
-    :param defaultPathName: is effect of the PathMethod in defaultPath,if the path is not exit of your computer,it has raise an error\n
+    :param defaultPathName: is effect of the PathMethod in defaultPath,if the path is not exit of your computer,
+    it has raise an error\n
     :param customerPath: if the path is not exit of your computer,is input has expected error
            else,you can download picture of your computer path
  """
     urlList = []
     methodList = ['workPath', 'defaultPath', 'customerPath']
+
     if PathMethod not in methodList:
-        typer.Exit("you can not chosen a method")
+        typer.Exit()
     else:
         for i in range(len(fatalist)):
             urlList.append(fatalist[i])
-        processPicture(urls=urlList, pathMethod=PathMethod, customerPath=customerPath, defaultPathName=defaultPathName)
+        processPicture(urls=urlList, pathMethod=PathMethod, customerPath=customerPath, defaultPathName=defaultPathName, Referer_wedsite=Referer_wedsite)
+
+
+
 @aqq.command()
-def watchMainDoc():
+def maindDoc():
     print(main.__doc__)
 
 
